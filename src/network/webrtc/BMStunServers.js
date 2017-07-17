f841dc215e983f955c596af0e5f9bbcf96c15b42
Steve Dekorte <steve@dekorte.com>
2017-07-17 12:16:49 -0700

    cleanup

--- src/network/webrtc/BMStunServers.js
+++ src/network/webrtc/BMStunServers.js
@@ -16,12 +16,6 @@ BMStunServers = BMStorableNode.extend().newSlots({
 		this.setSubnodeProto(BMStunServer)
     },
     
-    /*
-    subnodeProto: function() {
-        return BMStunServer
-    },
-    */
-    
     addServer: function (aServer) {
         return this.addSubnode(aServer)
     },
@@ -32,25 +26,25 @@ BMStunServers = BMStorableNode.extend().newSlots({
 
 	defaultOptions: function() {
 		return {'iceServers': [
-		    {url:'stun:stun01.sipphone.com'},
-		    {url:'stun:stun.ekiga.net'},
-		    {url:'stun:stun.fwdnet.net'},
-		    {url:'stun:stun.ideasip.com'},
-		    {url:'stun:stun.iptel.org'},
-		    {url:'stun:stun.rixtelecom.se'},
-		    {url:'stun:stun.schlund.de'},
-		    {url:'stun:stun.l.google.com:19302'},
-		    {url:'stun:stun1.l.google.com:19302'},
-		    {url:'stun:stun2.l.google.com:19302'},
-		    {url:'stun:stun3.l.google.com:19302'},
-		    {url:'stun:stun4.l.google.com:19302'},
-		    {url:'stun:stunserver.org'},
-		    {url:'stun:stun.softjoys.com'},
-		    {url:'stun:stun.voiparound.com'},
-		    {url:'stun:stun.voipbuster.com'},
-		    {url:'stun:stun.voipstunt.com'},
-		    {url:'stun:stun.voxgratia.org'},
-		    {url:'stun:stun.xten.com'},
+		    { url:"stun:stun01.sipphone.com" },
+		    { url:"stun:stun.ekiga.net" },
+		    { url:"stun:stun.fwdnet.net" },
+		    { url:"stun:stun.ideasip.com" },
+		    { url:"stun:stun.iptel.org" },
+		    { url:"stun:stun.rixtelecom.se" },
+		    { url:"stun:stun.schlund.de" },
+		    { url:"stun:stun.l.google.com:19302" },
+		    { url:"stun:stun1.l.google.com:19302" },
+		    { url:"stun:stun2.l.google.com:19302" },
+		    { url:"stun:stun3.l.google.com:19302" },
+		    { url:"stun:stun4.l.google.com:19302" },
+		    { url:"stun:stunserver.org" },
+		    { url:"stun:stun.softjoys.com" },
+		    { url:"stun:stun.voiparound.com" },
+		    { url:"stun:stun.voipbuster.com" },
+		    { url:"stun:stun.voipstunt.com" },
+		    { url:"stun:stun.voxgratia.org" },
+		    { url:"stun:stun.xten.com" },
 		]}	
 	},
 

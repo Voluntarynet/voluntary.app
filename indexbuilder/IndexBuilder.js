f5efed0f944594d381f6292e71a568116c392469
Steve Dekorte <steve@dekorte.com>
2019-03-26 17:55:09 -0700

    remove old vscode folder

--- indexbuilder/IndexBuilder.js
+++ indexbuilder/IndexBuilder.js
@@ -1,12 +1,13 @@
-//
-// Achive.js
-// 
-// This script builds a index.html file for the app
-// containing all the css and js file content.
-//
-// The resulting index.html (with the font and icon folders) 
-// should be able to run the app on it's own.
-//
+/*
+   Achive.js
+   
+   This script builds a index.html file for the app
+   containing all the css and js file content.
+  
+   The resulting index.html (with the font and icon folders) 
+   should be able to run the app on it's own.
+   
+*/
 
 // --- some helpful ultility methods ----
 

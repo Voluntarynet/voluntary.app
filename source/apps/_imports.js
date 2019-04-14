125dda29161cb695caa3c933ee284bfb78717d5d
Steve Dekorte <steve@dekorte.com>
2019-04-13 19:40:53 -0700

    app ref fix

--- source/apps/_imports.js
+++ source/apps/_imports.js
@@ -3,7 +3,7 @@
 ResourceLoader.pushRelativePaths([
     //"Atom/_imports.js",
     //"Game/_imports.js",
-    //"Peer/_imports.js",
-    "Clear/_imports.js",
+    "Peer/_imports.js",
+    //"Clear/_imports.js",
 ])
 
